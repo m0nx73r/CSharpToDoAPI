a45e54360af3f110ecda1c5dc621499ed8b2c83e
Aditya Kumar <naammkyahai@gmail.com>
2024-01-15 18:26:55 +0530

    Add project files.

---
+++ ToDoAPI/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Entities;
+
+namespace ToDoAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ToDoItem> TodoItems { get; set; }
+    }
+}
---
+++ ToDoAPI/Entities/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoAPI.Entities
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string? Task { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? CreatedUserId { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+        public string? ModifiedUserId { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+}
